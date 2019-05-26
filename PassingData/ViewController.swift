8b13b1db2eb37f4f07c4879d52fc5de427aba3ce
Greicy Pinedo <jpinedom@hotmail.com>
2019-05-26 11:09:40 -0500

    Initial Commit

---
+++ PassingData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassingData
+//
+//  Created by Jorge Pinedo on 5/26/19.
+//  Copyright © 2019 Jorge Pinedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
